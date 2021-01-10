3785633ad4db36a55e01ac9f85bc685adc9541fe
Christerle Nounoute <christerlesn@gmail.com>
2021-01-10 23:41:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/cart/)
       @@cart.each do |t|
         resp.write "#{t}\n"
+      end
       # if @@cart.empty?
       #   resp.write "Your cart is empty"
       # else
@@ -18,7 +19,7 @@ class Application
     elsif req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-      # end
+       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
