7e629fa22879e34d074645c85388c519967936f9
Christerle Nounoute <christerlesn@gmail.com>
2021-01-11 00:09:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,6 +25,13 @@ class Application
     else
       resp.write "Path Not Found"
     end
+
+  elsif req.path.match(/add/)
+    search_term = req.params["item"]
+    resp.write add_to_cart(search_term)
+    else
+    resp.write "We don't have that item"
+  end
     resp.finish
   end
 
