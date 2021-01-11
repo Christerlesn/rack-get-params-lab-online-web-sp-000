0bc63693fd83bd97c206c3b76b4f731025739091
Christerle Nounoute <christerlesn@gmail.com>
2021-01-11 00:21:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/cart/)
-      if !@@cart.empty
+      if !@@cart.empty?
         @@cart.each do |t|
           resp.write "#{t}\n"
         end
